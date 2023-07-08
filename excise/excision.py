3209a4c644fd233d7f434c8a501dfb2bea3afd78
viniciusmiradouro <vm@euclid.localdomain>
2023-07-08 09:52:10 -0300

    excision defined

---
+++ excise/excision.py
@@ -0,0 +1,18 @@
+from pydantic import FilePath, PositiveInt
+
+
+def excision(file_path: FilePath, start_line: PositiveInt, end_line: PositiveInt) -> None:
+    """
+    Extracts and prints an inclusive range of lines from a file.
+
+    Args:
+        file_path (FilePath): The path to the file.
+        start_line (PositiveInt): The starting line number (1-based).
+        end_line (PositiveInt): The ending line number (inclusive).
+
+    Returns:
+        None
+    """
+    lines = file_path.read_text().splitlines()
+    selected_lines = ''.join(lines[start_line - 1:end_line])
+    print(selected_lines)
